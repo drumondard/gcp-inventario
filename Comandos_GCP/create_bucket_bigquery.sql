2942ffe64462a84a5f2065125f585932ec035e74
Alexandre R Drumond <77971755+drumondard@users.noreply.github.com>
2025-05-28 08:23:16 -0300

    Google Cloud Storage (GCS)
    Google Cloud Storage (GCS)

--- Comandos_GCP/create_bucket_bigquery.sql
+++ Comandos_GCP/create_bucket_bigquery.sql
@@ -13,3 +13,26 @@ OPTIONS (
 
 CREATE OR REPLACE TABLE `vtal-sandbox-engenharia.inventario_comunicados_interrupcao.raw_comunicados_interrupcao_fe_24052025` AS
 SELECT * FROM `vtal-sandbox-engenharia.inventario_comunicados_interrupcao.temp_comunicados_interrupcao_fe_24052025`;
+
+------------------------------------------------------------------------------------------------------------
+/*
+CRIA TABELA NO BIGQUERY VIA GCS
+*/
+CREATE OR REPLACE EXTERNAL TABLE `vtal-sandbox-engenharia.inventario_comunicados_interrupcao.temp_comunicados_interrupcao_fe_27052025`
+OPTIONS ( 
+  allow_jagged_rows = TRUE, 
+  encoding = "UTF-8", 
+  field_delimiter = ";", 
+  skip_leading_rows = 1, 
+  ignore_unknown_values = TRUE, 
+  max_bad_records = 100, 
+  format = "CSV", 
+  uris = ["gs://vtal-land-zone-engenharia/Inventario/sci_comunicados/fechados/2025/202505/comunicados_interrupcao_fe_27052025.csv"]
+);
+
+CREATE OR REPLACE TABLE `vtal-sandbox-engenharia.inventario_comunicados_interrupcao.raw_comunicados_interrupcao_fe_27052025` AS
+SELECT *,
+  "comunicados_interrupcao_fe_27052025.csv" AS arquivo_origem,
+  TIMESTAMP("2025-05-27 06:09:04.623822 UTC") AS dt_insert
+FROM `vtal-sandbox-engenharia.inventario_comunicados_interrupcao.temp_comunicados_interrupcao_fe_27052025`;
+
